1493943b73b33cbe8b977a16a905a491cb5a15c3
Crxssbxw <144593744+crxssbxw@users.noreply.github.com>
2024-04-22 14:25:20 +0300

    Add files via upload

---
+++ L5_SP_2/L5_SP_2.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <fstream>
+#include <L5_SP/city.h>
+#include <L5_SP/citytime.h>
+#include <vector>
+
+using namespace std;
+
+void load(string filename, vector<City> &cities);
+void sort(vector<City> &cities, bool asc = true);
+
+int main() 
+{
+    system("chcp 1251");
+
+    vector<City> cities;
+    string filename;
+
+    cout << "Введите название файла без расширения и без посфикса \"_clear\":\n";
+    cin >> filename;
+    try {
+        load(filename, cities);
+    }
+    catch (exception e) {
+        cout << e.what() << endl;
+    }
+
+    sort(cities);
+
+    for (City& city : cities) {
+        cout << "----===== Город =====----\n"
+            << city.name << endl 
+            << "Основан " << city.foundation.GetDate() << endl
+            << "Население: " << city.population << " тыс. человек" << endl
+            << "Площадь: " << city.area << " кв. км" << endl;
+    }
+}
+
+void sort(vector<City> &cities, bool asc) {
+    if (asc == true) {
+        for (int i = 1; i < cities.size(); i++) {
+            City temp = cities[i];
+            int j = i - 1;
+            while (j >= 0 && (cities[j].population > temp.population || cities[j].area > temp.area)) {
+                cities[j + 1] = cities[j];
+                j--;
+            }
+            cities[j + 1] = temp;
+        }
+    }
+    else {
+        for (int i = 1; i < cities.size(); i++) {
+            City temp = cities[i];
+            int j = i - 1;
+            while (j >= 0 && (cities[j].population < temp.population || cities[j].area < temp.area)) {
+                cities[j + 1] = cities[j];
+                j--;
+            }
+            cities[j + 1] = temp;
+        }
+    }
+}
+
+void load(string filename, vector<City>& cities) {
+    string clearfile = filename + "_clear.txt";
+    string path = "..\\L5_SP\\" + clearfile;
+
+    ifstream read(path);
+
+    read.exceptions(ios::badbit | ios::failbit);
+
+    string name, date;
+    double population, area;
+
+    cout << "Файл открыт. Чтение...\n";
+    while (!read.eof() && read >> name >> date >> population >> area) {
+        City city(name, StringToDate(date), population, area);
+        cities.push_back(city);  
+    }
+
+    cout << "Чтение завершено. Добавлено " << cities.size() << " записей\n";
+    read.close();
+}
---
+++ Lab8_list/Lab8_list.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+#include <string>
+#include <L5_SP/city.h>
+#include <L5_SP/citytime.h>
+#include <L5_SP/cities_arr.h>
+
+int main() {
+	system("chcp 1251");
+
+	//Объявление списка
+	CitiesList list;
+
+	//Проверка на пустой список
+	cout << "Список пуст, если 1: " << list.is_empty() << endl;
+
+	//Создание экземпляра города
+	citytime date = StringToDate("01.01.903");
+
+	City city ("Moscow", date, 5000, 1600);
+	
+	//Добавление города в конец
+	list.AddBack(city);
+
+	//Повторно другой город
+	City city1 ("Vladimir", StringToDate("05.04.1342"), 400, 340);
+
+	list.AddBack(city1);
+
+	City city2("Kovrov", StringToDate("14.12.1567"), 300, 130);
+
+	//Добавление в указанную позицию
+	//С исключением
+	try {
+		list.AddPos(city2, 15);
+	}
+	catch (const char* message) {
+		cout << message << endl;
+	}
+	list.AddBack(city2);
+
+	//Без исключения
+	list.AddPos(city2, 1);
+
+	cout << endl;
+	//Вывод списка
+	list.print();
+
+	//Поиск элемента по позиции
+	cout << "Элемент на позиции 1: ";
+	City* findcity = list.find(1);
+
+	cout << findcity->name << endl;
+
+	//Поиск позиций с указанной датой
+	int* positions;
+	int lenght;
+
+	positions = list.FindByDate("14.12.1567", lenght);
+
+	cout << "\nПозиции по указанной дате (14.12.1567): ";
+	for (int i = 0; i < lenght; i++) {
+		cout << positions[i] << ", ";
+	}
+	cout << endl;
+
+	delete[] positions;
+
+	//Удаление
+	//С исключением
+	try {
+		list.Remove(15);
+	}
+	catch (const char* message) {
+		cout << message << endl;
+	}
+
+	//Без исключения
+	list.Remove(2);
+
+	//Вывод имен
+	cout << "\nВывод имен в массиве: " << endl;
+	string* names = list.GetNames();
+
+	for (int i = 0; i < list.GetLenght(); i++) {
+		cout << names[i] << endl;
+	}
+
+	delete[] names;
+
+	
+}
